624e247347066bb8b32180331181437529bae53f
alisarfraz13 <alisarfraz6411@gmail.com>
2025-12-25 21:59:43 +0500

    clean the functions

--- vars/buildDockerImage.groovy
+++ vars/buildDockerImage.groovy
@@ -3,12 +3,10 @@ def call(Map config) {
     def imageName     = config.imageName
     def buildTag      = config.buildTag
 
-    // Creds se Username nikal kr image build kr rhy hein
     withCredentials([usernamePassword(credentialsId: credentialsId, usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]) {
         def fullImageName = "${env.DOCKER_USER}/${imageName}"
         echo "🔨 Building Docker image: ${fullImageName}:${buildTag}"
         
-        // Local build (No Pull required later)
         sh "docker build . -t ${fullImageName}:${buildTag}"
         sh "docker tag ${fullImageName}:${buildTag} ${fullImageName}:latest"
     }
